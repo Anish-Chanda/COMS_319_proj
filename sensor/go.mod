382e55872f59a48a56d804ad2e3a5bb2182711c1
Anish Chanda <anish03@iastate.edu>
2024-03-10 12:18:52 -0500

    sensor alpha 0.0.3

--- sensor/go.mod
+++ sensor/go.mod
@@ -5,6 +5,12 @@ go 1.22.0
 require github.com/d2r2/go-dht v0.0.0-20200119175940-4ba96621a218
 
 require (
+	periph.io/x/conn/v3 v3.7.0 // indirect
+	periph.io/x/host/v3 v3.8.2 // indirect
+)
+
+require (
+	github.com/MichaelS11/go-dht v0.1.1
 	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22 // indirect
 	github.com/d2r2/go-shell v0.0.0-20211022052110-f591c27e3e2e // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- sensor/main.go
+++ sensor/main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	// "github.com/MichaelS11/go-dht"
-	"github.com/d2r2/go-dht"
+	"github.com/MichaelS11/go-dht"
+	// "github.com/d2r2/go-dht"
 )
 
 func main() {
+	const GPIO = "GPIO17"
+
+	hosterr := dht.HostInit()
+	if hosterr != nil {
+		fmt.Println("HostInit error:", hosterr)
+		return
+	}
+
+	dht, dhterr := dht.NewDHT(GPIO, dht.Fahrenheit, "")
+	if dhterr != nil {
+		fmt.Println("NewDHT error:", dhterr)
+		return
+	}
+
+	humidity, temperature, readerr := dht.Read()
+
+	if readerr != nil {
+		fmt.Println("Reader error:", readerr)
+		return
+	}
+
+	fmt.Printf("humidity: %v\n", humidity)
+	fmt.Printf("temperature: %v\n", temperature)
+
+	// Temperature from Raspberry Pi with Go
+
 	// err := dht.HostInit()
 	// if err != nil {
 	// 	fmt.Println("HostInit error:", err)
@@ -30,12 +55,12 @@ func main() {
 	// fmt.Printf("humidity: %v\n", humidity)
 	// fmt.Printf("temperature: %v\n", temperature)
 
-	temperature, humidity, retried, err :=
-		dht.ReadDHTxxWithRetry(dht.DHT11, 4, false, 10)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Print temperature and humidity
-	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-		temperature, humidity, retried)
+	// temperature, humidity, retried, err :=
+	// 	dht.ReadDHTxxWithRetry(dht.DHT11, 7, false, 10)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// // Print temperature and humidity
+	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+	// 	temperature, humidity, retried)
 }
