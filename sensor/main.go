e2573108063a339a50018409a4b2a97937e2ec8d
Anish Chanda <anish03@iastate.edu>
2024-03-11 12:29:53 -0500

    python script to read data

--- sensor/main.go
+++ sensor/main.go
@@ -4,63 +4,27 @@ import (
 	"fmt"
 
 	"github.com/MichaelS11/go-dht"
-	// "github.com/d2r2/go-dht"
 )
 
 func main() {
-	const GPIO = "GPIO17"
-
-	hosterr := dht.HostInit()
-	if hosterr != nil {
-		fmt.Println("HostInit error:", hosterr)
+	err := dht.HostInit()
+	if err != nil {
+		fmt.Println("HostInit error:", err)
 		return
 	}
 
-	dht, dhterr := dht.NewDHT(GPIO, dht.Fahrenheit, "")
-	if dhterr != nil {
-		fmt.Println("NewDHT error:", dhterr)
+	dht, err := dht.NewDHT("GPIO19", dht.Fahrenheit, "")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
 		return
 	}
 
-	humidity, temperature, readerr := dht.ReadRetry(11)
-
-	if readerr != nil {
-		fmt.Println("Reader error:", readerr)
+	humidity, temperature, err := dht.Read()
+	if err != nil {
+		fmt.Println("Read error:", err)
 		return
 	}
 
 	fmt.Printf("humidity: %v\n", humidity)
 	fmt.Printf("temperature: %v\n", temperature)
-
-	// Temperature from Raspberry Pi with Go
-
-	// err := dht.HostInit()
-	// if err != nil {
-	// 	fmt.Println("HostInit error:", err)
-	// 	return
-	// }
-
-	// dht, err := dht.NewDHT("GPIO19", dht.Fahrenheit, "")
-	// if err != nil {
-	// 	fmt.Println("NewDHT error:", err)
-	// 	return
-	// }
-
-	// humidity, temperature, err := dht.ReadRetry(11)
-	// if err != nil {
-	// 	fmt.Println("Read error:", err)
-	// 	return
-	// }
-
-	// fmt.Printf("humidity: %v\n", humidity)
-	// fmt.Printf("temperature: %v\n", temperature)
-
-	// temperature, humidity, retried, err :=
-	// 	dht.ReadDHTxxWithRetry(dht.DHT11, 7, false, 10)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// // Print temperature and humidity
-	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-	// 	temperature, humidity, retried)
 }
