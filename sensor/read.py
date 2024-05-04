0f1b3c48c3d6a6bb1509d8b4da879c668dc4d090
Anish Chanda <anish.pvt03@gmail.com>
2024-05-04 10:54:00 -0500

    calc average values in sensor readings

--- sensor/read.py
+++ sensor/read.py
@@ -3,14 +3,30 @@ import board
 import adafruit_dht
 #Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D17)
-while True:
+
+data = []
+
+while len(data) <5:
     try:
          # Print the values to the serial port
          temperature_c = dhtDevice.temperature
          temperature_f = temperature_c * (9 / 5) + 32
          humidity = dhtDevice.humidity
+         data.append((temperature_c,humidity))
          print("Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(temperature_f, temperature_c, humidity))
+         print("added successfull read")
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
     time.sleep(2.0)
+
+#calculate average of data
+avg_temp = (sum(temp for temp, _ in data)) / len(data)
+avg_hum = (sum(hum for _, hum in data)) / len(data)
+
+print("Average temp is: ", avg_temp)
+print("Average humidity is: ", avg_hum)
+
+#add to database
+
+
